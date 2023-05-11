100e404fb67d6702631603e0e2e770f98f33291f
William Fleming <sc20wrpf@leeds.ac.uk>
2023-05-11 19:54:37 +0100

    Populated database

--- bank/bank/views.py
+++ bank/bank/views.py
@@ -7,32 +7,32 @@ import json
 import requests
 import random
 
+def populateDatabase():
+    Currency.objects.create(code='GBP', symbol='£', exchange_rate=1.00)
 
-# Currency.objects.create(code='GBP', symbol='£', exchange_rate=1.00)
+    Currency.objects.create(code='USD', symbol='$', exchange_rate=1.38) 
+    Currency.objects.create(code='EUR', symbol='€', exchange_rate=1.17) 
+    Currency.objects.create(code='JPY', symbol='¥', exchange_rate=152.24)  
+    Currency.objects.create(code='AUD', symbol='$', exchange_rate=1.80) 
+    Currency.objects.create(code='CAD', symbol='$', exchange_rate=1.72)
+    Currency.objects.create(code='CHF', symbol='CHF', exchange_rate=1.27)
+    Currency.objects.create(code='CNY', symbol='¥', exchange_rate=8.78)
+    Currency.objects.create(code='SEK', symbol='kr', exchange_rate=11.92)
+    Currency.objects.create(code='NZD', symbol='$', exchange_rate=1.97)
 
-# Currency.objects.create(code='USD', symbol='$', exchange_rate=1.38) 
-# Currency.objects.create(code='EUR', symbol='€', exchange_rate=1.17) 
-# Currency.objects.create(code='JPY', symbol='¥', exchange_rate=152.24)  
-# Currency.objects.create(code='AUD', symbol='$', exchange_rate=1.80) 
-# Currency.objects.create(code='CAD', symbol='$', exchange_rate=1.72)
-# Currency.objects.create(code='CHF', symbol='CHF', exchange_rate=1.27)
-# Currency.objects.create(code='CNY', symbol='¥', exchange_rate=8.78)
-# Currency.objects.create(code='SEK', symbol='kr', exchange_rate=11.92)
-# Currency.objects.create(code='NZD', symbol='$', exchange_rate=1.97)
+    gbp = Currency.objects.get(code='GBP')
 
-# gbp = Currency.objects.get(code='GBP')
+    Account.objects.create(company_name='Company A', balance=10000.00, currency_id=gbp)
+    Account.objects.create(company_name='Company B', balance=20000.00, currency_id=gbp)
+    Account.objects.create(company_name='Company C', balance=30000.00, currency_id=gbp)
 
-# Account.objects.create(company_name='Company A', balance=10000.00, currency_id=gbp)
-# Account.objects.create(company_name='Company B', balance=20000.00, currency_id=gbp)
-# Account.objects.create(company_name='Company C', balance=30000.00, currency_id=gbp)
+    account_a = Account.objects.get(account_id=1)
+    account_b = Account.objects.get(account_id=2)
+    account_c = Account.objects.get(account_id=3)
 
-# account_a = Account.objects.get(account_id=1)
-# account_b = Account.objects.get(account_id=2)
-# account_c = Account.objects.get(account_id=3)
-
-# Transaction.objects.create(account_id=account_a, transaction_date=timezone.now(), transaction_amount=format(random.uniform(100, 1000), '.2f'), transaction_currency=gbp)
-# Transaction.objects.create(account_id=account_b, transaction_date=timezone.now(), transaction_amount=format(random.uniform(100, 1000), '.2f'), transaction_currency=gbp)
-# Transaction.objects.create(account_id=account_c, transaction_date=timezone.now(), transaction_amount=format(random.uniform(100, 1000), '.2f'), transaction_currency=gbp)
+    Transaction.objects.create(account_id=account_a, transaction_date=timezone.now(), transaction_amount=format(random.uniform(100, 1000), '.2f'), transaction_currency=gbp)
+    Transaction.objects.create(account_id=account_b, transaction_date=timezone.now(), transaction_amount=format(random.uniform(100, 1000), '.2f'), transaction_currency=gbp)
+    Transaction.objects.create(account_id=account_c, transaction_date=timezone.now(), transaction_amount=format(random.uniform(100, 1000), '.2f'), transaction_currency=gbp)
 
 
 class PayView(View):
@@ -40,6 +40,9 @@ class PayView(View):
         return super().dispatch(*args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        if not Account.objects.exists():
+            populateDatabase()
+
         # Parse the incoming JSON data
         data = json.loads(request.body)
 
@@ -161,6 +164,7 @@ class GetCurrencyExchangeView(View):
         return super().dispatch(*args, **kwargs)
 
     def get(self, request, from_currency, amount, *args, **kwargs):
+
         try:
             amount = float(amount)
         except ValueError:
